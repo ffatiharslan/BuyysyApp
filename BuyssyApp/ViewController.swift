b8ed82ecf826227c83f01427429b0b3813789df8
Fatih Arslan <fatiharslan1845@gmail.com>
2024-10-12 13:40:11 +0300

    initial commit

---
+++ BuyssyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuyssyApp
+//
+//  Created by fatih arslan on 12.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
