3ff6f246593fc8822fd2630a82a6b781a4aac73f
Fatih Arslan <fatiharslan1845@gmail.com>
2024-10-12 14:11:31 +0300

    created homepage collectionview

--- BuyssyApp/ui/screens/HomepageVC.swift
+++ BuyssyApp/ui/screens/HomepageVC.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class HomepageVC: UIViewController {
 
+    @IBOutlet weak var productsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        productsCollectionView.delegate = self
+        productsCollectionView.dataSource = self
+        
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        
+        let ekranGenislik = UIScreen.main.bounds.width
+        let itemGenislik = (ekranGenislik - 30) / 2
+        
+        layout.itemSize = CGSize(width: itemGenislik, height: itemGenislik * 1.7)
+        
+        productsCollectionView.collectionViewLayout = layout
     }
 
 
 }
 
+
+
+extension HomepageVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomepageCell", for: indexPath) as! HomepageCell
+        cell.productImageView.image = UIImage(systemName: "person")
+        cell.productNameLabel.text = "Deneme girişi"
+        
+        cell.layer.borderColor = UIColor.lightGray.cgColor
+        cell.layer.borderWidth = 0.5
+        cell.layer.cornerRadius = 10
+        
+        return cell
+    }
+}
---
+++ BuyssyApp/ui/tableview/HomepageCell.swift
@@ -0,0 +1,15 @@
+//
+//  HomepageCell.swift
+//  BuyssyApp
+//
+//  Created by fatih arslan on 12.10.2024.
+//
+
+import UIKit
+
+class HomepageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    
+}
